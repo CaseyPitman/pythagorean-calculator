dc07b2837da1fdcade978405eb9a3f8e0836518e
Casey The Great <casey@caseypitman.com>
2020-05-27 12:42:00 -0500

    clean up

--- js/main.js
+++ js/main.js
@@ -1,42 +1,31 @@
-
-$(document).ready(function() {
 //Calculator to find the unknown side of a right traingle via the Pythagorean Theorem
-
 //a^2 + b^2 = c^2
 
-
 //Button to activate the calculator
-  
 var onClick = document.getElementById("onClick");
-
+//Input not number
 var numberAlert = "You must enter side lengths as numbers.";
 
-
 //function that decides which math to do depending on user input
-
 function calculate(){
-  
   //gets input from the user
   var sideAInput = document.getElementById("side-a-input").value;
   var sideBInput = document.getElementById("side-b-input").value;
   var sideCInput = document.getElementById("side-c-input").value;
   
- 
   // Checks to be sure user only entered two sides
   function tooManyInputs(){ 
-   if (sideAInput.length > 0 && sideBInput.length > 0 && sideCInput.length > 0){
-  alert("Enter two sides only.");
-   }  
-  }//End input count check function
+    if (sideAInput.length > 0 && sideBInput.length > 0 && sideCInput.length > 0){
+      alert("Enter two sides only.");
+    }  
+  }
   
   //Calls check for number of inputs
   tooManyInputs();
 
-  
   var sideASq = 0;
   var sideBSq = 0;
   var sideCSq = 0;
-  
   
   //Finds A if unknown
   if (sideAInput.length === 0){ 
@@ -49,7 +38,6 @@ function calculate(){
     sideAInput = Math.sqrt(sideASq);
     document.getElementById("side-a-input").value= sideAInput;
   }
-  
   
   //Finds B if unknown
   else if (sideBInput.length === 0){
@@ -74,12 +62,10 @@ function calculate(){
     sideCInput = Math.sqrt(sideCSq);
     document.getElementById("side-c-input").value = sideCInput;
   }
-  
 }//ends Calculate function 
 
-  //calls the function calculate when clicked
-  	
-onClick.addEventListener("click", calculate);
+//calls the function calculate when clicked
+$("#onClick").on("click", calculate);
 
 //resets the input fields
 var startOver = document.getElementById("reset");
@@ -92,4 +78,3 @@ function clearInput(){
 
 startOver.addEventListener("click", clearInput);
 
-});
